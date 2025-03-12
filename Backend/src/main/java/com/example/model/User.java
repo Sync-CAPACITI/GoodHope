33b36e92cc204c011eef5e98155e963bcf517512
Sinawo Mngxuma <sinawomngxuma@gmail.com>
2025-03-12 14:14:48 +0200

    installed npm, added node modules to .gitinore file

--- Backend/src/main/java/com/example/model/User.java
+++ Backend/src/main/java/com/example/model/User.java
@@ -2,6 +2,8 @@ package com.example.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Setter;
+
 import java.util.List;
 
 @Data
@@ -15,6 +17,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer userId;
 
+    @Setter
     private String name;
     private String email;
     private String phoneNumber;
@@ -42,48 +45,24 @@ public class User {
         return userId;
     }
 
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPhoneNumber() {
         return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
     }
 
     public String getAddress() {
         return address;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
     public Role getRole() {
         return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
     }
 
     public Address getAddressDetails() {
