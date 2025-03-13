aade1c34fe52b57e1cc714d2b7db4f58b0a4217b
Zolwethu <zithasbu621@gmail.com>
2025-03-13 08:39:22 +0200

    landing restyle for cohesion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,8 @@ function App() {
     <div className="App">
       
       <div>
-        <ParentDashboard/>
+        {/* <ParentDashboard/> */}
+        <LandingPage/>
       </div>
     </div>
   );
